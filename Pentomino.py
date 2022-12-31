8d604910df70ddd66f629a373f6a955669cff5c7
Johnathan Cassady <jcassady@andrew.cmu.edu>
2022-12-30 18:01:06 -0800

    started user inputs

--- Pentomino.py
+++ Pentomino.py
@@ -12,3 +12,26 @@ class Pentomino:
         self.objectMatrix = Pentomino.pentominoData.get("pentominoObjectMatrixData").get(objectId)
         self.orientation = [0,0,1]
     
+    def renderCube(self, pentominoesInstance, pos, cubeArray, parentNode):
+        # Load the environment model.
+        distanceScale = 2
+        distanceOffset = 1
+        newCube = pentominoesInstance.loader.loadModel("./eggFiles/cube")
+        # Reparent the model to render.
+        newCube.reparentTo(parentNode)
+        # Apply scale and position transforms on the model.
+        newCube.setScale(1, 1, 1)
+        newCube.setPos(\
+            distanceScale*pos[0]+distanceOffset, \
+            distanceScale*pos[1]+distanceOffset, \
+            distanceScale*pos[2]+distanceOffset)
+        cubeArray.append(newCube)
+
+    def renderPentomino(self, pentominoesInstance):
+        #Render a specific pentomino Object
+        objectNode = pentominoesInstance.render.attachNewNode(self.objectId)
+        cubeArray = []
+        for cubeLocation in self.objectMatrix:
+            Pentomino.renderCube(self, pentominoesInstance, cubeLocation, cubeArray, objectNode)
+        pentominoesInstance.renderedObjects[self.objectId] = cubeArray
+        return objectNode
--- Pentominoes.py
+++ Pentominoes.py
@@ -1,21 +1,30 @@
 from math import pi, sin, cos
 
+import numpy as np
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 
 from panda3d.core import *
+from panda3d.core import LVecBase3
 
 from Pentomino import Pentomino
 
+
 class Pentominoes(ShowBase):
+    def moveSelectedObjectUp(self):
+        self.selectedNodePos.addY(1)
 
-    def test(self):
-        Pentomino0 = Pentomino("1")
+    def moveSelectedObjectDown(self):
+        self.selectedNodePos.addY(-1)
 
-        Pentominoes.renderObject(self, Pentomino0, [0, 0, 0, 0], [0, 0, 0])
+    def moveSelectedObjectLeft(self):
+        self.selectedNodePos.addX(-1)
 
-        print("hi")
+    def moveSelectedObjectRight(self):
+        self.selectedNodePos.addX(1)
 
+    def cycleSelectedObject(self):
+        self.selectedNode.setColorScale(100, 1, 1, 1)
 
     def __init__(self):
         ShowBase.__init__(self)
@@ -27,10 +36,23 @@ class Pentominoes(ShowBase):
         Pentominoes.initLighting(self)
         self.renderOrigin()
 
-        self.accept('arrow_up-up', self.test)
+        #x y z i j k
+        self.selectedNodePos = LVecBase3()
+        self.selectedNodeOrient = LVecBase3()
+
+        Pentomino0 = Pentomino("0")
+        self.selectedNode = Pentomino0.renderPentomino(self)
+
+        self.accept('arrow_up', self.moveSelectedObjectUp)
+        self.accept('arrow_down', self.moveSelectedObjectDown)
+        self.accept('arrow_left', self.moveSelectedObjectLeft)
+        self.accept('arrow_right', self.moveSelectedObjectRight)
+
+        self.accept('t', self.cycleSelectedObject)
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        self.taskMgr.add(self.updateSelectedObjectPosTask, "updateSelectedObjectPosTask")
 
     def initLighting(self):
         ambientLight = AmbientLight('ambientLight')
@@ -50,28 +72,6 @@ class Pentominoes(ShowBase):
         plnp2.setPos(100, -50, 100)
         self.render.setLight(plnp2)
 
-    def renderCube(self, pos, cubeArray):
-        # Load the environment model.
-        distanceScale = 2
-        distanceOffset = 1
-        newCube = self.loader.loadModel("./eggFiles/cube")
-        # Reparent the model to render.
-        newCube.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        newCube.setScale(1, 1, 1)
-        newCube.setPos(\
-            distanceScale*pos[0]+distanceOffset, \
-            distanceScale*pos[1]+distanceOffset, \
-            distanceScale*pos[2]+distanceOffset)
-        cubeArray.append(newCube)
-
-    def renderObject(self, pentominoObject, orientation, position):
-        #Render a specific pentomino Object
-        cubeArray = []
-        for cubeLocation in pentominoObject.objectMatrix:
-            Pentominoes.renderCube(self, cubeLocation, cubeArray)
-        self.renderedObjects[pentominoObject.objectId] = cubeArray
-
     def renderOrigin(self):
         self.origin = self.loader.loadModel("./eggFiles/Axes")
         self.origin.reparentTo(self.render)
@@ -82,9 +82,14 @@ class Pentominoes(ShowBase):
     def spinCameraTask(self, task):
         angleDegrees = task.time * 20.0
         angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setPos(40 * sin(angleRadians), -40 * cos(angleRadians), 3)
         self.camera.setHpr(angleDegrees, 0, 0)
         return Task.cont
+
+    def updateSelectedObjectPosTask(self, task):
+        self.selectedNode.setPosHpr(self.selectedNodePos, self.selectedNodeOrient)
+        return Task.cont
+
 
 pentominoesInstance = Pentominoes()
 # pentominoesInstance.test()
